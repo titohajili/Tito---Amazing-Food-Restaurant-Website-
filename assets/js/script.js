90174ebbae8b431c43e7a0daad45766a55f25391
titohajili <maqatoto963@gmail.com>
2024-01-12 23:13:10 +0400

    The header will be visible when you go down

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,15 +19,35 @@ const addEventonElements = function (elements, eventType, callback) {
 
 
 //      NAVBAR----------------------
-
-const navbar = document.querySelector('[data-navbar]');
-const navTogglers = document.querySelectorAll('[data-nav-toggler]');
-const overlay = document.querySelector('[data-overlay]');
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
 
 const toggleNavbar = function () {
-    navbar.classList.toggle('active');
-    overlay.classList.toggle('active');
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
     document.body.classList.toggle("nav-active");
 }
 
-addEventonElements(navTogglers, 'click', toggleNavbar);
+navTogglers.forEach(function(navToggler) {
+    navToggler.addEventListener("click", toggleNavbar);
+});
+
+//    HEADER---------------
+
+const header = document.querySelector('[data-header]');
+
+let lastScrollpos = 0;
+
+const hideHeader = function() {
+    
+}
+
+window.addEventListener('scroll', function (){
+    if (this.window.scrollY > 50) {
+        header.classList.add("active");
+    } else{
+        header.classList.remove("active");
+    }
+})
+
