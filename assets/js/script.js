da237db22873709f1213cd3bc43fc8a0f273caf9
titohajili <maqatoto963@gmail.com>
2024-01-12 16:32:21 +0400

    ADD EVENT LISTENER ON MULTIPLE ELEMENTS

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,24 @@ window.addEventListener('load', function () {
     preloader.classList.add('loaded');
     this.document.body.classList.add('loaded');
 })
+/* ADD EVENT LISTENER ON MULTIPLE ELEMENTS */
+const addEventonElements = function (elements, eventType, callback) {
+    for(let i = 0, len = elements.lenght; i < len; i++ ) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+
+//      NAVBAR----------------------
+
+const navbar = document.querySelector('[data-navbar]');
+const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+const overlay = document.querySelector('[data-overlay]');
+
+const toggleNavbar = function () {
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle("nav-active");
+}
+
+addEventonElements(navTogglers, 'click', toggleNavbar);
