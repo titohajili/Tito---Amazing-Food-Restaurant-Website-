3dba2fb6fe8e516ee1114723aa34d51314c45aea
titohajili <maqatoto963@gmail.com>
2024-01-13 23:55:46 +0400

    Hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,15 +37,24 @@ navTogglers.forEach(function(navToggler) {
 
 const header = document.querySelector('[data-header]');
 
-let lastScrollpos = 0;
+let lastScrollPos = 0;
 
 const hideHeader = function() {
-    
+    const isScrollBottom = lastScrollPos < window.scrollY;
+    if (isScrollBottom) {
+        header.classList.add("hide");
+
+    } else {
+        header.classList.remove("hide");
+    }
+
+    lastScrollPos = window.scrollY;
 }
 
 window.addEventListener('scroll', function (){
     if (this.window.scrollY > 50) {
         header.classList.add("active");
+        hideHeader();
     } else{
         header.classList.remove("active");
     }
